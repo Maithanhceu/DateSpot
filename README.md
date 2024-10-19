61b71a46cae25f99143d7f313f22f8dd227bf4bb
Mai <ttmai@my.loyno.edu>
2024-10-18 20:16:52 -0400

    clean up my server links ensuring that they all have their own individual sections divided by external API calls than API calls to my date_spot database, added a links to my pitch presentation && trello board to my README.md, editd my db.sql file again to ensure the structure to my tables are setup correctly., and downloaded more dependencies

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Date Spot
 1. Project Overview
 - **Date Spot **is a dating platform/application inspired by conversations between** Mia and Mai**, two single friends navigating the challenges of finding emotionally available partners in the fast-paced environment of Brooklyn. After experiencing emotional burnout from the typical online dating experience, they came up with the idea for date spot. Their goal is to create a solution that addresses the prevalent issues in modern dating culture, including overwhelming choices, catfishing, ghosting, and more.
+
+Please find the link to my project timeline here: ![link to my Trello](https://trello.com/b/rTnpw6dE/datespot)
+
+And you can see my entire proposal here: ![Link to my proposal](https://docs.google.com/document/d/1XYBEou6I9RMuMyFoCIXmRGw1BHPGT_y8BgiAnVQBWL4/edit?usp=sharing)
--- Server/db.sql
+++ Server/db.sql
@@ -1,51 +1,263 @@
--- Create the Users table
-CREATE TABLE Users (
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    email VARCHAR(255)
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.13 (Homebrew)
+-- Dumped by pg_dump version 14.13 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: events; Type: TABLE; Schema: public; Owner: thanhmai
+--
+
+CREATE TABLE public.events (
+    eventid integer NOT NULL,
+    creatorid integer,
+    date date NOT NULL,
+    location character varying(255),
+    eventtype character varying(255),
+    eventdescription text NOT NULL,
+    eventtitle character varying(255),
+    eventphoto character varying(255)
 );
 
--- Create the Events table
-CREATE TABLE Events (
-    eventId SERIAL PRIMARY KEY,
-    creatorId INT REFERENCES Users(userId),
-    date DATE NOT NULL,
-    location VARCHAR(255),
-    eventType VARCHAR(255),
-    eventDescription TEXT NOT NULL,
-    eventTitle VARCHAR(255),
-    eventPhoto VARCHAR(255)
+
+ALTER TABLE public.events OWNER TO thanhmai;
+
+--
+-- Name: events_eventid_seq; Type: SEQUENCE; Schema: public; Owner: thanhmai
+--
+
+CREATE SEQUENCE public.events_eventid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.events_eventid_seq OWNER TO thanhmai;
+
+--
+-- Name: events_eventid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: thanhmai
+--
+
+ALTER SEQUENCE public.events_eventid_seq OWNED BY public.events.eventid;
+
+
+--
+-- Name: userevents; Type: TABLE; Schema: public; Owner: thanhmai
+--
+
+CREATE TABLE public.userevents (
+    usereventid integer NOT NULL,
+    userid integer,
+    eventid integer,
+    date date,
+    location character varying(255),
+    eventtype character varying(255),
+    eventdescription text,
+    eventtitle character varying(255),
+    eventphoto character varying(255)
 );
 
--- Create the UserEvents table with additional event detail columns
-CREATE TABLE UserEvents (
-    userEventId SERIAL PRIMARY KEY,
-    userId INT REFERENCES Users(userId),
-    eventId INT REFERENCES Events(eventId),
-    date DATE,
-    location VARCHAR(255),
-    eventType VARCHAR(255),
-    eventDescription TEXT,
-    eventTitle VARCHAR(255),
-    eventPhoto VARCHAR(255)
+
+ALTER TABLE public.userevents OWNER TO thanhmai;
+
+--
+-- Name: userevents_usereventid_seq; Type: SEQUENCE; Schema: public; Owner: thanhmai
+--
+
+CREATE SEQUENCE public.userevents_usereventid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.userevents_usereventid_seq OWNER TO thanhmai;
+
+--
+-- Name: userevents_usereventid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: thanhmai
+--
+
+ALTER SEQUENCE public.userevents_usereventid_seq OWNED BY public.userevents.usereventid;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: thanhmai
+--
+
+CREATE TABLE public.users (
+    userid integer NOT NULL,
+    username character varying(255) NOT NULL,
+    password character varying(255) NOT NULL,
+    firstname character varying(255),
+    lastname character varying(255),
+    email character varying(255)
 );
 
--- Insert example data into Users and Events tables, and link them in UserEvents
-WITH inserted_user AS (
-    INSERT INTO Users (username, password, firstName, lastName, email)
-    VALUES ('mai', 'maifantastic', 'Mai', 'M.', 'ttmai@my.loyno.edu')
-    RETURNING userId
-),
-inserted_event AS (
-    INSERT INTO Events (creatorId, date, location, eventType, eventDescription, eventTitle, eventPhoto)
-    SELECT userId, '2024-11-13', 'Brooklyn', 'Comedy Show', 'A fun comedy show', 'Live, Laugh, Love', '/Comedy_show.jpg'
-    FROM inserted_user
-    RETURNING eventId, date, location, eventType, eventDescription, eventTitle, eventPhoto
-)
-INSERT INTO UserEvents (userId, eventId, date, location, eventType, eventDescription, eventTitle, eventPhoto)
-SELECT inserted_user.userId, inserted_event.eventId, inserted_event.date, inserted_event.location, 
-       inserted_event.eventType, inserted_event.eventDescription, inserted_event.eventTitle, inserted_event.eventPhoto
-FROM inserted_user, inserted_event;
+
+ALTER TABLE public.users OWNER TO thanhmai;
+
+--
+-- Name: users_userid_seq; Type: SEQUENCE; Schema: public; Owner: thanhmai
+--
+
+CREATE SEQUENCE public.users_userid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_userid_seq OWNER TO thanhmai;
+
+--
+-- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: thanhmai
+--
+
+ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
+
+
+--
+-- Name: events eventid; Type: DEFAULT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.events ALTER COLUMN eventid SET DEFAULT nextval('public.events_eventid_seq'::regclass);
+
+
+--
+-- Name: userevents usereventid; Type: DEFAULT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.userevents ALTER COLUMN usereventid SET DEFAULT nextval('public.userevents_usereventid_seq'::regclass);
+
+
+--
+-- Name: users userid; Type: DEFAULT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN userid SET DEFAULT nextval('public.users_userid_seq'::regclass);
+
+
+--
+-- Data for Name: events; Type: TABLE DATA; Schema: public; Owner: thanhmai
+--
+
+COPY public.events (eventid, creatorid, date, location, eventtype, eventdescription, eventtitle, eventphoto) FROM stdin;
+1	1	2024-11-13	Brooklyn	Comedy Show	A fun comedy show	Live, Laugh, Love	/Comedy_show.jpg
+\.
+
+
+--
+-- Data for Name: userevents; Type: TABLE DATA; Schema: public; Owner: thanhmai
+--
+
+COPY public.userevents (usereventid, userid, eventid, date, location, eventtype, eventdescription, eventtitle, eventphoto) FROM stdin;
+1	1	1	2024-11-13	Brooklyn	Comedy Show	A fun comedy show	Live, Laugh, Love	/Comedy_show.jpg
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: thanhmai
+--
+
+COPY public.users (userid, username, password, firstname, lastname, email) FROM stdin;
+1	mai	maifantastic	Mai	M.	ttmai@my.loyno.edu
+3	kim4possible	callmebeepme	kim	possible	kim4possible@gmail.com
+\.
+
+
+--
+-- Name: events_eventid_seq; Type: SEQUENCE SET; Schema: public; Owner: thanhmai
+--
+
+SELECT pg_catalog.setval('public.events_eventid_seq', 2, true);
+
+
+--
+-- Name: userevents_usereventid_seq; Type: SEQUENCE SET; Schema: public; Owner: thanhmai
+--
+
+SELECT pg_catalog.setval('public.userevents_usereventid_seq', 9, true);
+
+
+--
+-- Name: users_userid_seq; Type: SEQUENCE SET; Schema: public; Owner: thanhmai
+--
+
+SELECT pg_catalog.setval('public.users_userid_seq', 3, true);
+
+
+--
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_pkey PRIMARY KEY (eventid);
+
+
+--
+-- Name: userevents userevents_pkey; Type: CONSTRAINT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.userevents
+    ADD CONSTRAINT userevents_pkey PRIMARY KEY (usereventid);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (userid);
+
+
+--
+-- Name: events events_creatorid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT events_creatorid_fkey FOREIGN KEY (creatorid) REFERENCES public.users(userid);
+
+
+--
+-- Name: userevents userevents_eventid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.userevents
+    ADD CONSTRAINT userevents_eventid_fkey FOREIGN KEY (eventid) REFERENCES public.events(eventid);
+
+
+--
+-- Name: userevents userevents_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: thanhmai
+--
+
+ALTER TABLE ONLY public.userevents
+    ADD CONSTRAINT userevents_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
