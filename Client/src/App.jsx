75113639f390f15e1a2f5c8e4a7e789bbf42acca
Mai <ttmai@my.loyno.edu>
2024-10-21 17:54:23 -0400

    Add routing for UsersPage, HomePage, NoPage, AboutPage, CodeOfConductPage, and EventsPage. Verified that all pages render correctly.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
-import NewsAPI from './Components/NewsAPI';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './Pages/About';
+import Home from './Pages/Home';
+import UsersPage from './Pages/UsersPage';
+import CodeOfConduct from './Pages/CodeOfConduct';
+import NoPage from './Pages/NoPage'
+import EventsPage from './Pages/EventsPage';
 
 function App() {
   return (
-    <><div>App</div>
-    <div><NewsAPI /></div></>
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home/>} />
+            <Route path="/Home" element={<Home/>} />
+            <Route path="/About" element={<About/>} />
+            <Route path="/EventsPage" element={<EventsPage/>} />
+            <Route path="/CodeOfCoduct" element={<CodeOfConduct/>} />
+            <Route path="/UsersPage" element={<UsersPage/>} />
+            <Route path="*" element={<NoPage/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   )
 }
 
---
+++ Client/src/Pages/EventsPage.jsx
@@ -0,0 +1,8 @@
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>Events Page</h1>
+          
+        </div>
+    );
+}
