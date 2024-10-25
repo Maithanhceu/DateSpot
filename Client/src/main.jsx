c844f54f255809a2e92a6dbe8f4059f7d5b2d4c5
Mai <ttmai@my.loyno.edu>
2024-10-25 00:37:42 -0400

    Add login and logout buttons using Auth0
    - Implemented LoginButton component for user authentication
    - Created LogOutButton component for user sign-out
    - Integrated Auth0 hooks for handling authentication state

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,8 +4,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const domain =import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId =import.meta.env.VITE_CLIENT_ID;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider 
+    domain ={domain} 
+    clientId={clientId} 
+    redirectUri={window.location.origin}
+    >
+
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
