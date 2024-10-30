6e8c3601a273b6ffbad8c4682cfd97e809957af0
Mai <ttmai@my.loyno.edu>
2024-10-30 15:20:55 -0400

    fixed file import and deleted an unnecessary file

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -3,13 +3,14 @@ import NewsAPI from '../Components/NewsAPI';
 import EventForm from '../Components/EventCreation/EventForm';
 
 
+
 export default function Home() {
     return (
         <>
             <AltTextProvider>
                 <div className='row-container'>
-                <EventForm/>
                 <NewsAPI/>
+                <EventForm/>
                 </div>
             </AltTextProvider>
 
