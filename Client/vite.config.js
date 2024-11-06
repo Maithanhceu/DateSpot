e6f2c5e331bde118e6bfdc23694a724497600fc1
Mai <ttmai@my.loyno.edu>
2024-11-05 21:37:09 -0500

    added in database variable to data_spot database to Railway

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,23 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/news/romance': { target: 'http://localhost:1113', changeOrigin: true },
-      '/uploadPhoto': { target: 'http://localhost:1113', changeOrigin: true },
-      '/events': { target: 'http://localhost:1113', changeOrigin: true },
-      '/register': { target: 'http://localhost:1113', changeOrigin: true },
-      '/altText': { target: 'http://localhost:1113', changeOrigin: true },
-      '/deleteEvent': { target: 'http://localhost:1113', changeOrigin: true },
-      '/editEvents': { target: 'http://localhost:1113', changeOrigin: true },
-      '/userEventsTable': { target: 'http://localhost:1113', changeOrigin: true },
-      '/deleteUserEvent': { target: 'http://localhost:1113', changeOrigin: true },
-    },
-  },
   test: {
-    globals: true,
+    globals: true, 
     environment: 'jsdom',
-  },
+},
 });
-
 
