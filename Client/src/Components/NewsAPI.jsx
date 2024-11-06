849c91058cd2af20837d1e662baee5da788dff76
Mai <ttmai@my.loyno.edu>
2024-11-06 00:13:21 -0500

    debugging the fetch url

--- Client/src/Components/NewsAPI.jsx
+++ Client/src/Components/NewsAPI.jsx
@@ -7,7 +7,7 @@ function NewsAPI() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('/news/romance');
+                const response = await fetch('https://datespot-production.up.railway.app/news/romance');
                 const result = await response.json();
                 setNewData(result);
             } catch (error) {
