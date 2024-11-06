4b5a706a9a7997441adbb5a4b1ad03173f416305
Mai <ttmai@my.loyno.edu>
2024-11-05 21:37:09 -0500

    added in database variable to data_spot database to Railway

--- Server/server.js
+++ Server/server.js
@@ -18,10 +18,11 @@ app.use(express.json());
 app.use(cors());
 
 const pool = new Pool({
-    user: 'thanhmai',
-    host: 'localhost',
-    database: 'date_spot',
-    port: 5432,
+    user: process.env.DB_USER,        
+    host: process.env.DB_HOST,        
+    database: process.env.DB_NAME,    
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT || 5432, 
 });
 
 // -------------------------------------------------------------------------------------------------
