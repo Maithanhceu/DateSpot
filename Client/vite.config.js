359ffc27dd917ebc19fdfd5defd4f3fa32e18794
Mai <ttmai@my.loyno.edu>
2024-11-06 19:46:08 -0500

    keep vite.config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,9 +3,25 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   test: {
     globals: true, 
     environment: 'jsdom',
 },
+=======
+  server: {
+    proxy: {
+      '/news/romance': { target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/uploadPhoto': { target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/events': { target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/register': { target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/altText': { target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/deleteEvent':{ target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/editEvents/':{ target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/userEventsTable/':{ target: 'datespot-production.up.railway.app', changeOrigin: true },
+      '/deleteUserEvent/':{ target: 'datespot-production.up.railway.app', changeOrigin: true },
+    },
+  },
+>>>>>>> 507d4e2 (deleted an extra db.sql)
 });
 
